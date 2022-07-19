07d7b1d86688bab5ed4e159090a5b0775972c093
Lenovo <Lenovo@DESKTOP-VIEUAQD.mshome.net>
2022-07-20 05:24:08 +0530

    framework created

---
+++ TestYantraSampleProject/src/test/java/sampleclasses/Log_Inpages.java
@@ -0,0 +1,26 @@
+package sampleclasses;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Log_Inpages 
+{
+@Test
+	
+	public void practice4Test()
+	{
+		Reporter.log("Tyss-->test4",true);
+		String url = System.getProperty("URL");
+		System.out.println("url");
+		String pwd = System.getProperty("PASSWORD");
+		System.out.println("pwd");
+		String hii = System.getProperty("hii");
+		System.out.println("good evening");
+		System.out.println("good Anuprita Raut123");
+		System.out.println("good morning");
+		System.out.println("success");
+		  
+		
+	}
+
+}
